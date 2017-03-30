921665d67d63948e3a1834ab70c1cb2dbc53d581
softpo <455098435@qq.com>
2017-03-30 12:56:36 +0800

    获取文件信息

--- app/src/main/java/com/softpo/requestingasharedfile/MainActivity.java
+++ app/src/main/java/com/softpo/requestingasharedfile/MainActivity.java
@@ -1,9 +1,12 @@
 package com.softpo.requestingasharedfile;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -19,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public void request(View view) {
         Intent requestIntent = new Intent(Intent.ACTION_PICK);
         requestIntent.setType("image/*");
-        this.startActivityForResult(requestIntent,101);
+        this.startActivityForResult(requestIntent, 101);
     }
+
     //获取数据的返回值处理
     @Override
     public void onActivityResult(int requestCode, int resultCode,
@@ -34,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
             Uri returnUri = returnIntent.getData();
             ImageView showImg = (ImageView) findViewById(R.id.showImg);
             showImg.setImageURI(returnUri);
+
+            Cursor returnCursor =
+                    getContentResolver().query(returnUri, null, null, null, null);
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+            returnCursor.moveToFirst();
+            String fileName = returnCursor.getString(nameIndex);
+            long fileSize = returnCursor.getLong(sizeIndex);
+            Log.d("flag", "----------------->onActivityResult: fileName: "+fileName+", fileSize: "+fileSize);
+
         }
     }
 }
